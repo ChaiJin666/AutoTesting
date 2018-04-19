79764b068b9635c0a268cb41752e3b546ebb0870
chaijin <chaijin@chaijindemacbook.local>
2018-04-19 16:03:29 +0800

    string learning

---
+++ src/main/java/StringDemo01.java
@@ -0,0 +1,7 @@
+public class StringDemo01 {
+    public static void main(String args[]){
+        char[] helloArray ={'c','h','a','i','j','i','n'};
+        String hellostring =new String(helloArray);
+        System.out.println(hellostring);
+    }
+}
